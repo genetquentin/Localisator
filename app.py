e2260febf79b467d58b9cd94420bb2068c2b0f92
Quentin Genet <genetquentin@gmail.com>
2020-05-15 08:07:00 +0200

    Added css and a geolocate_page

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/geolocate')
+def geolocate_page():
+    return render_template('geolocate_page.html')
+
+
 
 if __name__ == '__main__':
     app.run()
